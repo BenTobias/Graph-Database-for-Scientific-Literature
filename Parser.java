c2b92e7559ca7c2c93dc6f0cb27a629ccbe274d5
Benedict Liang <benedictljj@gmail.com>
2014-03-22 18:55:39 +0800

    Refactored.

--- Parser.java
+++ Parser.java
@@ -18,7 +18,8 @@ public class Parser {
 
     private ArrayList<String> linksToCrawl = new ArrayList<String>();
 
-    public JSONObject getPaperJson(String html, String uriHost, String uriQuery) {
+    public JSONObject getPaperJson(String html, String uriHost,
+                                   String uriQuery) {
         JSONObject json = new JSONObject();
 
         String doi = uriQuery.split("=")[1];
@@ -27,7 +28,21 @@ public class Parser {
         String paperAbstract = doc.select("div#abstract p").html();
         Elements citations = doc.select("div#citations tr a");
 
-        ArrayList<Map<String, String>> citationsMapList = new ArrayList<Map<String, String>>();
+        ArrayList<Map<String, String>> citationsMapList =
+                getCitationURLMapList(uriHost, citations);
+
+        json.put("doi", doi);
+        json.put("abstract", paperAbstract);
+        json.put("citations", citationsMapList);
+
+        System.out.println(json);
+        return json;
+    }
+
+    private ArrayList<Map<String, String>> getCitationURLMapList(
+            String uriHost, Elements citations) {
+        ArrayList<Map<String, String>> citationsMapList =
+                new ArrayList<Map<String, String>>();
 
         for (Element c : citations) {
             String citationLink = uriHost + c.attr("href");
@@ -38,19 +53,12 @@ public class Parser {
             citationMap.put("title", citationTitle);
             citationsMapList.add(citationMap);
 
-            System.out.println(citationTitle);
             if (!c.hasClass("citation_only")) {
                 // Add to links to crawl array.
                 linksToCrawl.add(citationLink);
             }
         }
-
-        json.put("doi", doi);
-        json.put("abstract", paperAbstract);
-        json.put("citations", citationsMapList);
-
-        System.out.println(json);
-        return json;
+        return citationsMapList;
     }
 
     public static void main(String[] args) {
