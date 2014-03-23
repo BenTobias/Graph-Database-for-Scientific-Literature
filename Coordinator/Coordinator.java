b035f324be1137db672b8d12b9c71eb9f33aacf0
BlacKeNinG <blackening@gmail.com>
2014-03-23 10:38:39 +0800

    PUT_URLS is now working. (without URL sanitization)

--- Coordinator/Coordinator.java
+++ Coordinator/Coordinator.java
@@ -9,12 +9,20 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
+import java.util.Date;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 
 
 
+
+
+
+
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
 //refactor later.
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
@@ -39,6 +47,9 @@ import com.mongodb.util.JSON;
  * 		-It will only give u things which are neither "recently allocated" nor "already crawled"
  *  Secondly, you can tell it things you have discovered, mainly new targets
  *  	-It will add it to the list of things to crawl, if not already crawled.
+ *  
+ *  This unit will ensure that every URL is crawled only once.
+ *  	-Subsequent modifications will allow crawled URLs to expire, allowing recrawl.
  */
 
 /*
@@ -46,12 +57,20 @@ import com.mongodb.util.JSON;
  * http://www.javaworld.com/article/2073344/core-java/use-select-for-high-speed-networking.html
  * Note: "significant code reuse"
  */
+
+/*
+ * PROTOCOL:
+ * Query format: {cmd: "PUT_URLS"|"GET_URLS", URLS: ["string",...], limit: (int, >0)}
+ * URLS is not used for GET_URLS.
+ * limit is optional for GET_URLS. Not used for PUT_URLS.
+ */
 public class Coordinator {
 	
 	static int port = 15001;
 	private static final ByteBuffer buffer = ByteBuffer.allocate( 60000 );
 	static private Charset cs = Charset.forName("ASCII");
     static CharsetDecoder decoder = cs.newDecoder();
+    private static DBhelper db = new DBhelper();
 	
 	/*
 	 * This looks scary, but this basically grabs any connection runs the function process
@@ -121,8 +140,13 @@ public class Coordinator {
 		{e.printStackTrace();}
 	}
 	
+	/*
+	 * See top for PROTOCOL
+	 * 
+	 */
 	static boolean process(SocketChannel sc) throws Exception
 	{
+		//TODO: check correctness of write commands in non-blocking mode.
 		buffer.clear();
 	    int t = sc.read( buffer );
 	    buffer.flip();
@@ -138,11 +162,41 @@ public class Coordinator {
 	    DBObject msg = (DBObject)JSON.parse(s);
 	    System.out.println(msg);
 	    
-	    //printByteBuffer(buffer);
-	    //Check message type, there are 2 types of messages.
-	    	//PUT_URLS
+	  //Check message type, there are 2 types of messages.
+	    if(msg.get("cmd").toString().equals("PUT_URLS") )
+	    {
+	    	BasicDBList  urls = ((BasicDBList )msg.get("URLS")); //i hope this works.
+	    	DBCollection papers_db = db.GetCollection("PaperNodes");
+	    	for(int i = 0;i<urls.size();i++)
+	    	{
 	    		//TODO: RF URLs
-	    		//Retrieve and add if not already inside.
+	    		//add if not already inside.
+	    		DBCursor papers = db.GetPaperByUrl((String)urls.get(i));
+	    		if(papers.count() == 1)
+	    			continue;
+	    		if(papers.count() == 0)
+	    		{
+	    			//create and insert.
+	    			DBObject obj = new BasicDBObject();
+	    			obj.put("URL", (String)urls.get(i));
+	    			obj.put("Crawl_status", "Uncrawled");
+	    			obj.put("Crawl_date", new Date(0)); //this is basically, much long time ago. So we can do comparisons
+	    			//actually insert into database.
+	    			papers_db.insert(obj);
+	    		}
+	    		else
+	    			assert(false); //impossible to have more than 1 identical URL.
+	    	}
+	    }else if(msg.get("cmd").toString().equals("GET_URLS")){
+	    	
+	    }else
+	    {
+	    	System.out.println("Invalid command: '"+msg.get("cmd")+"'");
+	    	return false;
+	    }
+	    
+	    		
+	    		
 	    	//GET_URLS
 	    		//Retrieve N URLs from database, 
 	    			//give it to them and mark as "Tentative"
